c0d1193e65fe64f31d1c585de2aaac1708a5f1dc
Cauê Prado <caue.prado0@gmail.com>
2024-05-18 10:36:02 -0300

    refactor: the command messages

--- src/stopwatch/stopwatch.py
+++ src/stopwatch/stopwatch.py
@@ -13,12 +13,25 @@ class Stopwatch:
             self.start_time = datetime.now() - self.elapsed_time
             self.running = True
             click.echo("Stopwatch started")
+        else:
+            elapsed_time = datetime.now() - self.start_time
+            click.echo(
+                "The timer is already running. \n"
+                "You can stop, restart or display the elapsed time."
+            )
+            Stopwatch.print_elapsed_time(elapsed_time)
 
     def stop(self):
         if self.running:
             self.elapsed_time = datetime.now() - self.start_time
             self.running = False
-            click.echo(f"Stopwatch stopped at {self.elapsed_time}")
+            click.echo(f"The stopwatch was stopped at: {self.elapsed_time}")
+        else:
+            click.echo(
+                "The stopwatch is already stopped."
+                "You can resume, reset, or display the elapsed time."
+            )
+            Stopwatch.print_elapsed_time(self.elapsed_time)
 
     def reset(self):
         self.start_time = None
@@ -28,10 +41,16 @@ class Stopwatch:
 
     def display(self):
         if self.running:
-            elapsed = datetime.now() - self.start_time
+            elapsed_time = datetime.now() - self.start_time
         else:
-            elapsed = self.elapsed_time
-        click.echo(f"Elapsed time: {elapsed}")
+            elapsed_time = self.elapsed_time
+        Stopwatch.print_elapsed_time(elapsed_time)
+
+    @staticmethod
+    def print_elapsed_time(elapsed_time: timedelta):
+        click.echo(
+            f"The current elapsed time is: {elapsed_time}"
+        )
 
 
 stopwatch = Stopwatch()
